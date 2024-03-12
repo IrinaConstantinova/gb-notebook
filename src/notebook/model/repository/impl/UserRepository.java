4d5c145ab0c9a5f69ffae2d724c5fb00cec13abf
Irina.Constantinova <irina.constantinova@gmail.com>
2024-03-12 19:28:10 +0300

    Deleted DAO

--- src/notebook/model/repository/impl/UserRepository.java
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -1,26 +1,28 @@
 package notebook.model.repository.impl;
 
-import notebook.model.dao.impl.FileOperation;
+
+import notebook.util.DBConnector;
 import notebook.util.mapper.impl.UserMapper;
 import notebook.model.User;
 import notebook.model.repository.GBRepository;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepository implements GBRepository {
     private final UserMapper mapper;
-    private final FileOperation operation;
 
-    public UserRepository(FileOperation operation) {
+
+    public UserRepository() {
         this.mapper = new UserMapper();
-        this.operation = operation;
+
     }
 
     @Override
     public List<User> findAll() {
-        List<String> lines = operation.readAll();
+        List<String> lines = readAll();
         List<User> users = new ArrayList<>();
         for (String line : lines) {
             users.add(mapper.toOutput(line));
@@ -57,9 +59,15 @@ public class UserRepository implements GBRepository {
                 .filter(u -> u.getId()
                         .equals(userId))
                 .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
-        editUser.setFirstName(update.getFirstName());
-        editUser.setLastName(update.getLastName());
-        editUser.setPhone(update.getPhone());
+        if (!update.getFirstName().isEmpty()){
+            editUser.setFirstName(update.getFirstName());
+        }
+        if (!update.getLastName().isEmpty()){
+            editUser.setLastName(update.getLastName());
+        }
+        if (!update.getPhone().isEmpty()){
+            editUser.setPhone(update.getPhone());
+        }
         write(users);
         return Optional.of(update);
     }
@@ -74,7 +82,50 @@ public class UserRepository implements GBRepository {
         for (User u: users) {
             lines.add(mapper.toInput(u));
         }
-        operation.saveAll(lines);
+        saveAll(lines);
+    }
+
+    @Override
+    public List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(DBConnector.DB_PATH);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    @Override
+    public void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(DBConnector.DB_PATH, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
